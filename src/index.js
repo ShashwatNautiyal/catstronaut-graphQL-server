398e5acc6614870533df7391769122e6490727c5
Shashwat Nautiyal <shashwatnautiyal2015@gmail.com>
2022-05-09 10:03:56 +0530

    index file updated

--- src/index.js
+++ src/index.js
@@ -3,18 +3,23 @@ const typeDefs = require("./schema");
 const resolvers = require("./resolvers");
 const TrackAPI = require("./datasources/track-api");
 
-const server = new ApolloServer({
-	typeDefs,
-	resolvers,
-	dataSources: () => {
-		return { trackAPI: new TrackAPI() };
-	},
-});
+async function startApolloServer(typeDefs, resolvers) {
+	const server = new ApolloServer({
+		typeDefs,
+		resolvers,
+		dataSources: () => {
+			return {
+				trackAPI: new TrackAPI(),
+			};
+		},
+	});
 
-const { url, port } = await server.listen({ post: process.env.PORT || 4000 });
-
-console.log(`
-        🚀  Server is running!
+	const { url, port } = await server.listen();
+	console.log(`
+        🚀  Server is running
         🔉  Listening on port ${port}
         📭  Query at ${url}
-    `);
+      `);
+}
+
+startApolloServer(typeDefs, resolvers);
